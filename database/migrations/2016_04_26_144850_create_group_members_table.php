ad6ee1fd7de2473ba55af0c9ef0c4342456a9e47
Sereek <sereek@localhost>
2016-04-26 21:15:48 +0600

    Added migration files

--- database/migrations/2016_04_26_144850_create_group_members_table.php
+++ database/migrations/2016_04_26_144850_create_group_members_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGroupMembersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('group_members', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role_id');
-            $table->rememberToken();
+            $table->integer('group_id');
+            $table->integer('user_id');
+            $table->integer('privilege_id');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('group_members');
     }
 }
--- database/migrations/2016_04_26_145006_create_tasks_groups_relations_table.php
+++ database/migrations/2016_04_26_145006_create_tasks_groups_relations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTasksGroupsRelationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks_groups_relations', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role_id');
-            $table->rememberToken();
+            $table->integer('group_id');
+            $table->integer('task_id');
+            $table->integer('creator_id');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('tasks_users_relations');
     }
 }
